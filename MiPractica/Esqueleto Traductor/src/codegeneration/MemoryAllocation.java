2b29653c5268d58974f6406b7c1f30c9a6edd191
alexl0 <alexleon8898@gmail.com>
2021-05-25 19:35:15 +0200

    Mejorar codigo: 	- MemoryAllocation.java 	- abstracta 	- pdfs especificacion de codigo 	- arreglar test Falla: 	- errores y warnings en PL12/Test1.txt en GVM.exe (aunque el programa funciona) 		IMPORTANTE (solo en ese test) el < funciona como > y viceversa 	- errores y warnings en PL12/Test3.txt

--- MiPractica/Esqueleto Traductor/src/codegeneration/MemoryAllocation.java
+++ MiPractica/Esqueleto Traductor/src/codegeneration/MemoryAllocation.java
@@ -24,57 +24,45 @@ public class MemoryAllocation extends DefaultVisitor {
 
     public Object visit(StructDefinition node, Object param) {
         super.visit(node, param);
-
-        int address = 0;
-
-        for (StructField child : node.getDefinitions()) {
-            address += (int) child.accept(this, address);
-        }
-
+        int addr = 0;
+        for (StructField child : node.getDefinitions())
+            addr = addr + (int) child.accept(this, addr);
         return null;
     }
 
     public Object visit(StructField node, Object param) {
         super.visit(node, param);
-
-        int address = 0;
-
-        if (param instanceof Integer)
-            address = (int) param;
-
-        node.setAddress(address);
-
+        int addr = 0;
+        if (param instanceof Integer) {
+            addr = (int) param;
+        }
+        node.setAddress(addr);
         return node.getType().getSize();
     }
 
     public Object visit(VarDefinition node, Object param) {
         super.visit(node, param);
-
         if (node.getScope().equals(VarScope.GLOBAL)) {
             node.setAddress(currentAddress);
-            currentAddress += node.getType().getSize();
+            currentAddress = currentAddress + node.getType().getSize();
         }
-
         return null;
     }
 
     public Object visit(FunDefinition node, Object param) {
         super.visit(node, param);
-
-        int bp = 4;
-
-        for (int i = node.getParams().size() - 1; i >= 0; i--) {
-            node.getParams().get(i).setAddress(bp);
-            bp += node.getParams().get(i).getType().getSize();
+        int BP = 4;
+        int i = node.getParams().size() - 1;
+        while (i >= 0) {
+            node.getParams().get(i).setAddress(BP);
+            BP += node.getParams().get(i).getType().getSize();
+            i--;
         }
-
-        bp = 0;
-
+        BP = 0;
         for (VarDefinition child : node.getDefinitions()) {
-            bp -= child.getType().getSize();
-            child.setAddress(bp);
+            BP = BP - child.getType().getSize();
+            child.setAddress(BP);
         }
-
         return null;
     }
 
